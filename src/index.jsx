f4da224a3f93bd9cc4cc03be7df5caf9c9cd750c
Yao Wang <yao422321065@yahoo.com>
2019-01-03 22:06:32 -0500

    init

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/index.jsx";
+import "../assets/app.scss";
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
